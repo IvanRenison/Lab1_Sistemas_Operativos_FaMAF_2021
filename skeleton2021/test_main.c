d6cc6727ebecd590cc4f7b262ebd4eb51f22a271
tfuentes <tiffanybricett_1996@gmail.com>
2021-08-26 19:27:35 -0300

    test_main.c
    Se modica el test_main.c y se añade info al informe

--- skeleton2021/test_main.c
+++ skeleton2021/test_main.c
@@ -4,27 +4,36 @@
 
 
 int main(void) {
-    char * arg = "exec";
-    char * redir_in = "text.in";
-    char * redir_out = "text.out";
+    char * arg1 = strdup("exec");
+    char * arg2 = strdup("wc");
+    char * redir_in = strdup("text.in");
+    char * redir_out = strdup("text.out");
     char * test1 = NULL;
     char * test2 = NULL;
+    char * command = NULL;
 
     scommand comando = scommand_new();
-    scommand_push_back(comando, arg);
+    scommand_push_back(comando, arg1);
+    scommand_push_back(comando, arg2);
+    scommand_set_redir_in(comando, redir_in);
+    scommand_set_redir_out(comando, redir_out);
+    command = scommand_to_string(comando);
+    printf("%s\n", command);
     printf("%d\n", scommand_length(comando));
     char * front = scommand_front(comando);
     printf("%s\n", front);
     front = front;
     scommand_pop_front(comando);
-    scommand_set_redir_in(comando, redir_in);
-    scommand_set_redir_out(comando, redir_out);
     test1 = scommand_get_redir_in(comando);
     test2 = scommand_get_redir_out(comando);
     printf("%s\n", test1);
     printf("%s\n", test2);
     printf("%d\n", scommand_length(comando));
     comando = scommand_destroy(comando);
+    free(command);
+    free(arg1);
+    free(arg2);
+
 
     return(1);
 }
