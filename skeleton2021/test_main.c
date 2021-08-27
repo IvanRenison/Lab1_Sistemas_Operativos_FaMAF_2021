4d0764913ecbca5b9937a28f7606a73ad681d547
Iván <ivan.renison@mi.unc.edu.ar>
2021-08-27 17:26:00 -0300

    test

--- skeleton2021/test_main.c
+++ skeleton2021/test_main.c
@@ -4,55 +4,11 @@
 
 
 int main(void) {
-    scommand comando = scommand_new();
+    pipeline comandos = pipeline_new();
 
-    char* word = strdup("Hola");
-    scommand_push_back(comando, word);
-    word = strdup("mundo");
-    scommand_push_back(comando, word);
-    word = strdup("-Wall");
-    scommand_push_back(comando, word);
+    
 
-    word = strdup("in.in");
-    scommand_set_redir_in(comando, word);
-    word = strdup("out.out");
-    scommand_set_redir_out(comando, word);
-
-    printf("%s\n", scommand_front(comando));
-
-    char* comandoLindo = scommand_to_string(comando);
-    printf("%s\n", comandoLindo);
-    comando = scommand_destroy(comando);
-    free(comandoLindo); comandoLindo = NULL;
-
-/*     char * arg1 = strdup("exec");
-    char * arg2 = strdup("wc");
-    char * redir_in = strdup("text.in");
-    char * redir_out = strdup("text.out");
-    char * test1 = NULL;
-    char * test2 = NULL;
-    char * command = NULL;
-
-    scommand comando = scommand_new();
-    scommand_push_back(comando, arg1);
-    scommand_push_back(comando, arg2);
-    scommand_set_redir_in(comando, redir_in);
-    scommand_set_redir_out(comando, redir_out);
-    command = scommand_to_string(comando);
-    printf("%s\n", command);
-    printf("%d\n", scommand_length(comando));
-    char * front = scommand_front(comando);
-    printf("%s\n", front);
-    scommand_pop_front(comando);
-    test1 = scommand_get_redir_in(comando);
-    test2 = scommand_get_redir_out(comando);
-    printf("%s\n", test1);
-    printf("%s\n", test2);
-    printf("%d\n", scommand_length(comando));
-    comando = scommand_destroy(comando);
-    free(command); */
-
-
+    comandos = pipeline_destroy(comandos);
 
     return(EXIT_SUCCESS);
 }
