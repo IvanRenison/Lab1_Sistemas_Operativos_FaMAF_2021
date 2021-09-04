5a4d5dfee0943bc9119f33856b1f639caabad696
Iván <ivan.renison@mi.unc.edu.ar>
2021-09-04 12:46:48 -0300

    execute_multiple_pipelines

--- skeleton2021/execute.c
+++ skeleton2021/execute.c
@@ -1,96 +1,16 @@
 #include <assert.h>
 #include <fcntl.h> // open
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #include "builtin.h"
 #include "command.h"
 #include "execute.h"
 
-void execute_pipeline(pipeline apipe) {
-    assert(apipe != NULL);
-    unsigned int cantidadDeHijos = 0u;
-    pid_t pid;
-    scommand comando;
-
-    while (pipeline_length(apipe) > 1) {
-        comando = pipeline_front_and_pop(apipe);
-
-        if(scommand_is_empty(comando)){
-            comando = scommand_destroy(comando);
-        } else {
-            int pipefd[2];
-            int res_pipe = pipe(pipefd);
-            if (res_pipe < 0) {
-                perror("pipe: ");
-                return;
-            }
-
-            int punta_lectura = pipefd[0];
-            int punta_escritura = pipefd[1];
-
-            pid = fork();
-            if (pid < 0 ) {
-                perror("fork: ");
-                return;
-            }
-
-            if (pid == 0) {
-                // El proceso hijo
-                close(punta_lectura);
-                dup2(punta_escritura, STDOUT_FILENO);
-                close(punta_escritura);
-                scommand_exec(comando);
-                // Esto no se debería ejecutar
-                _exit(1);
-            }
-            else {
-                // El proceso padre
-                cantidadDeHijos++;
-                // Se deja la punta de lectura modificada para el proximo ciclo
-                close(punta_escritura);
-                dup2(punta_lectura, STDIN_FILENO);
-                close(punta_lectura);
-                comando = scommand_destroy(comando);
-            }
-        }
-    }
-    // Acá queda un solo comando
-    comando = pipeline_front_and_pop(apipe);
-    if(scommand_is_empty(comando)){
-        comando = scommand_destroy(comando);
-    } else {
-        pid = fork();
-
-        if (pid < 0 ) {
-            perror("fork: ");
-            return;
-        }
-
-        if (pid == 0) {
-            // EL proceso hijo
-            scommand_exec(comando);
-            // Esto no se debería ejecutar
-            _exit(1);
-        }
-        else {
-           // El proceso padre
-            cantidadDeHijos++;
-            comando = scommand_destroy(comando);
-
-        while (cantidadDeHijos > 0) {
-            wait(NULL);
-            cantidadDeHijos--;
-        }
-
-        }
-    }
-}
-
-
-/* Pone, si está seeteado, el archivo de redireción de entrada en el stdin,
+/* Pone, si está seeteado, el archivo de redirección de entrada en el stdin,
  * si no está seeteado no hace nada
  * Returns: 0 si la operación fue exitosa
  *          1 si la operación falló
@@ -102,7 +22,7 @@ static int change_file_descriptor_in(scommand cmd) {
     assert(cmd != NULL);
 
     char* redir_in = scommand_get_redir_in(cmd);
-    // si la redireción de entrada no está seeteada, scommand_get_redir_in
+    // si la redirección de entrada no está seeteada, scommand_get_redir_in
     // devuelve NULL
     if (redir_in != NULL) {
         int file_redir_in = open(redir_in, O_RDONLY);
@@ -129,14 +49,13 @@ static int change_file_descriptor_in(scommand cmd) {
             perror("");
             return (-1);
         }
-
     }
     return (0);
 }
 
-/* Pone, si está seeteado, el archivo de redireción de salida en el stdout,
+/* Pone, si está seeteado, el archivo de redirección de salida en el stdout,
  * si el archivo en existe lo crea
- * Si la redireción de salida no está seeteada no hace nada
+ * Si la redirección de salida no está seeteada no hace nada
  * Returns: 0 si la operación fue exitosa
  *          1 si la operación falló
  *
@@ -147,7 +66,7 @@ static int change_file_descriptor_out(scommand cmd) {
     assert(cmd != NULL);
 
     char* redir_out = scommand_get_redir_out(cmd);
-    // si la redireción de salida no está seeteada, scommand_get_redir_out devuelve NULL
+    // si la redirección de salida no está seeteada, scommand_get_redir_out devuelve NULL
     if (redir_out != NULL) {
 
         /*   open como segundo parametro toma flags, en este caso tiene los
@@ -194,7 +113,7 @@ static int change_file_descriptor_out(scommand cmd) {
  * fork, redirigiendo el stdin y el stdout si están seeteados
  * Puede modificar cmd, pero no destruirlo
  * Si la llamada sale bien no se retorna, si la llamada sale mal, si hay algún
- * problema con la redireción de entrada o de salida se retorna -1, y si la llamada
+ * problema con la redirección de entrada o de salida se retorna -1, y si la llamada
  * al programa falla se retorna el código de error
  * En caso de fallar la llamada al programa, los descriptores de archivo quedan cambiados
  *
@@ -204,13 +123,13 @@ static int change_file_descriptor_out(scommand cmd) {
 static int scommand_exec_external(scommand cmd) {
     assert(cmd != NULL && !scommand_is_empty(cmd));
 
-    // Se cambia stdin por el archivo de redireción de entrada, si es que está seeteado
+    // Se cambia stdin por el archivo de redirección de entrada, si es que está seeteado
     int exit_redir_in = change_file_descriptor_in(cmd);
     if (exit_redir_in != 0) {
         return (-1);
     }
 
-    // Se cambia stdout por el archivo de redireción de salida, si es que está seeteado
+    // Se cambia stdout por el archivo de redirección de salida, si es que está seeteado
     int exit_redir_out = change_file_descriptor_out(cmd);
     if (exit_redir_out != 0) {
         return (-1);
@@ -226,14 +145,15 @@ static int scommand_exec_external(scommand cmd) {
 
     unsigned int j = 0u;
     while (argv[j] != NULL) {
-        free(argv[j]); argv[j] = NULL;
+        free(argv[j]);
+        argv[j] = NULL;
         j++;
     }
-    free(argv); argv = NULL;
+    free(argv);
+    argv = NULL;
 
     return (ret_code);
 }
-
 
 /* Ejecuta un comando, tanto si es interno como si es externo
  * En el caso de ser externo solo retorno si hay un error, y en ese caso,
@@ -241,22 +161,130 @@ static int scommand_exec_external(scommand cmd) {
  * Puede modificar cmd, pero no destruirlo
  * 
  * En caso de fallar la llamada al programa, los descriptores de archivo pueden quedar
- * cambiados por los de redireción de entrada y de salida
+ * cambiados por los de redirección de entrada y de salida
  *
  * Requires: cmd != NULL && !scommand_is_empty(cmd)
  */
-int scommand_exec(scommand cmd) {
+static int scommand_exec(scommand cmd) {
     assert(cmd != NULL && !scommand_is_empty(cmd));
     int ret_code = 0;
     if (builtin_scommand_is_internal(cmd)) {
         builtin_scommand_exec(cmd);
         ret_code = 0;
-    }
-    else {
+    } else {
         ret_code = scommand_exec_external(cmd);
     }
 
     return (ret_code);
 }
 
+/* Ejecuta un pipeline no vacio, creando procesos hijos incluso para los comandos internos
+ * En caso de algún error deja de ejecutar.
+ * Modifica apipe, ya que le va sacando los comandos, y si no hay ningún error,
+ * lo deja vacio
+ * 
+ * Requires: apipe != NULL && !pipeline_is_empty(apipe)
+ *
+ */
+static void execute_multiple_pipelines(pipeline apipe) {
+    assert(apipe != NULL && !pipeline_is_empty(apipe));
 
+    unsigned int cantidadDeHijos = 0u;
+    bool error_flag = false;
+    // Variable para saber si hubo un error
+    // Si está en false es porque no hubo errores, si se vuelve true es porque si hubo
+
+    while (!error_flag && pipeline_length(apipe) > 1) {
+        scommand comando = pipeline_front_and_pop(apipe);
+
+        if (!scommand_is_empty(comando)) {
+            int pipefd[2];
+            int res_pipe = pipe(pipefd);
+            if (res_pipe < 0) {
+                perror("pipe");
+                error_flag = true;
+            }
+            else {
+                int punta_lectura = pipefd[0];
+                int punta_escritura = pipefd[1];
+
+                pid_t pid = fork();
+                if (pid < 0) {
+                    perror("fork");
+                    error_flag = true;
+                } else if (pid == 0) {
+                    // El proceso hijo
+                    close(punta_lectura);
+                    dup2(punta_escritura, STDOUT_FILENO);
+                    close(punta_escritura);
+                    scommand_exec(comando);
+                    // Esto no se debería ejecutar
+                    _exit(EXIT_FAILURE);
+                }
+                else {
+                    // El proceso padre
+                    cantidadDeHijos++;
+                    // Se deja la punta de lectura modificada para el proximo ciclo
+                    close(punta_escritura);
+                    dup2(punta_lectura, STDIN_FILENO);
+                    close(punta_lectura);
+                }
+            }
+        }
+        comando = scommand_destroy(comando);
+    }
+    if (!error_flag) {
+        // Acá queda un solo comando
+        // Ya que el while se ejecuto hasta que pipeline_length(apipe) = 1
+        scommand comando = pipeline_front_and_pop(apipe);
+        if (!scommand_is_empty(comando)) {
+            pid_t pid = fork();
+            if (pid < 0) {
+                perror("fork");
+                error_flag = true;
+            } else if (pid == 0) {
+                // EL proceso hijo
+                scommand_exec(comando);
+                // Esto no se debería ejecutar
+                _exit(EXIT_FAILURE);
+            }
+            else {
+                // El proceso padre
+                cantidadDeHijos++;
+            }
+        }
+        comando = scommand_destroy(comando);
+    }
+
+    // Se espera a que todos los proseso hijos terminen
+    while (cantidadDeHijos > 0) {
+        wait(NULL);
+        cantidadDeHijos--;
+    }
+}
+
+void execute_pipeline(pipeline apipe) {
+    assert(apipe != NULL);
+    // Checkear el tamaño de la pipe
+    unsigned int N = pipeline_length(apipe);
+    // Un solo comando
+    if (N == 1) {
+        // Ejecutar el comando
+        printf("Ejecutar el comando...\n");
+        if (builtin_scommand_is_single_internal(apipe)) {
+            builtin_single_pipeline_exec(apipe);
+        } else {
+            // Proceso hijo
+            int pid = fork();
+            if (pid < 0) {
+                perror("fork");
+            } else if (pid == 0) {
+                scommand_exec(pipeline_front(apipe));
+            }
+        }
+    } // Varios comandos
+    else {
+        // Recorrer los comandos y ejecutarlos
+        // Checkear si es necesario esperar un comando
+    }
+}
