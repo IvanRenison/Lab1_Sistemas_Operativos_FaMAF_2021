9de541daf86f7f6c859d3b684ce89e6ee233480c
Iván <ivan.renison@mi.unc.edu.ar>
2021-09-04 11:28:18 -0300

    multiple_pipelines2

--- skeleton2021/test_main.c
+++ skeleton2021/test_main.c
@@ -38,6 +38,8 @@ void pipeline2(scommand cmd1, scommand cmd2) {
     else {
         // El padre
         close(punta_escitura);
+        dup2(punta_lectura, STDIN_FILENO);
+        close(punta_lectura);
         int pid2 = fork();
         if (pid2 == -1) {
             // Caso de error
@@ -46,8 +48,6 @@ void pipeline2(scommand cmd1, scommand cmd2) {
         else if (pid2 == 0) {
             // El hijo
             // Acá se lee de la punta de lectura
-            dup2(punta_lectura, STDIN_FILENO);
-            close(punta_lectura);
             scommand_exec(cmd2);
         }
         else {
@@ -126,22 +126,100 @@ void multiple_pipelines(pipeline p){
     free(pipefds);
 }
 
+// Para multiple comandos (mas de 2)
+static void multiple_pipelines2(pipeline p) {
+    unsigned int cantidadDeHijos = 0u;
+    pid_t pid;
+    scommand comando;
+
+    while (pipeline_length(p) > 1) {
+        comando = pipeline_front_and_pop(p);
+        int pipefd[2];
+        int res_pipe = pipe(pipefd);
+        assert(res_pipe == 0); ///
+
+        int punta_lectura = pipefd[0];
+        int punta_escitura = pipefd[1];
+
+        pid = fork();
+        assert(pid >= 0);
+
+        if (pid == 0) {
+            // El proceso hijo
+            close(punta_lectura);
+            dup2(punta_escitura, STDOUT_FILENO);
+            close(punta_escitura);
+            scommand_exec(comando);
+            // Esto no se debería ejecutar
+            exit(EXIT_FAILURE); ////
+        }
+        else {
+            // El proceso padre
+            cantidadDeHijos++;
+            // Se deja la punta de lectura modificada para el proximo ciclo
+            close(punta_escitura);
+            dup2(punta_lectura, STDIN_FILENO);
+            close(punta_lectura);
+            comando = scommand_destroy(comando);
+        }
+    }
+    // Acá queda un solo comando
+    comando = pipeline_front_and_pop(p);
+    pid = fork();
+    assert(pid >= 0);
+
+    if (pid == 0) {
+        // EL proceso hijo
+        scommand_exec(comando);
+        // Esto no se debería ejecutar
+        exit(EXIT_FAILURE); ////
+    }
+    else {
+        // El proceso padre
+        cantidadDeHijos++;
+        comando = scommand_destroy(comando);
+
+        while (cantidadDeHijos > 0) {
+            wait(NULL);
+            cantidadDeHijos--;
+        }
+
+    }
+    
+}
+
+
+
 
 int main(void) {
 
-    /*scommand comando1 = scommand_new();
-    scommand_push_back(comando1, strdup("ls -l"));
+    scommand comando1 = scommand_new();
+    scommand_push_back(comando1, strdup("ls"));
 
     scommand comando2 = scommand_new();
-    scommand_push_back(comando2, strdup("wc -l"));
+    scommand_push_back(comando2, strdup("cd"));
     scommand_push_back(comando2, strdup("c"));
 
-    pipeline2(comando1, comando2);
+    scommand comando3 = scommand_new();
+    scommand_push_back(comando3, strdup("wc"));
+    scommand_push_back(comando3, strdup("-l"));
 
-    comando1 = scommand_destroy(comando1);
-    comando2 = scommand_destroy(comando2);*/
+    pipeline p = pipeline_new();
+    pipeline_push_back(p, comando1);
+    pipeline_push_back(p, comando2);
+    pipeline_push_back(p, comando3);
 
-    pipeline pipe = pipeline_new();
+    multiple_pipelines2(p);
+
+    p = pipeline_destroy(p);
+
+/*     char** argv = calloc(3, sizeof(char));
+    argv[0] = strdup("ctweqewtqd");
+    argv[1] = strdup("dafsf");
+    argv[2] = NULL;
+    execvp(argv[0], argv); */
+
+/*     pipeline pipe = pipeline_new();
     scommand comando1 = scommand_new();
     scommand comando2 = scommand_new();
     scommand comando3 = scommand_new();
@@ -153,7 +231,7 @@ int main(void) {
     pipeline_push_back(pipe, comando2);
     pipeline_push_back(pipe, comando3);
     multiple_pipelines(pipe);
-    pipeline_destroy(pipe);
+    pipeline_destroy(pipe); */
 
     return (EXIT_SUCCESS);
 }
