88020fb54e2593aa0e2a81133ad1cd1c32d86355
Iván <ivan.renison@mi.unc.edu.ar>
2021-09-05 18:04:51 -0300

    Colores en el prompt

--- skeleton2021/prompt.c
+++ skeleton2021/prompt.c
@@ -5,6 +5,15 @@
 
 #include "prompt.h"
 
+/* Para imprimir con color se usan códigos ANSI de la forma
+   "\033[38;2;rojo;verde;azulm"
+*/
+
+#define ANSI_NOCOLOR "\033[0m" // Esto vuelve al color por defecto
+
+#define ANSI_GREEN "\033[38;2;0;255;0m"
+#define ANSI_BLUE "\033[38;2;14;112;255m"
+
 void show_prompt(void) {
     char hostname[256];
     hostname[255] = '\0'; // El último caracter es NULL
@@ -18,6 +27,10 @@ void show_prompt(void) {
     workdir[2555] = '\0';
     getcwd(workdir, 2556);
 
-    printf("%s@%s/mybash>%s$ ", password->pw_name, hostname, workdir);
+    printf(ANSI_GREEN "%s@%s" ANSI_NOCOLOR "/mybash>" ANSI_BLUE
+                      "%s" ANSI_NOCOLOR "$ ",
+           password->pw_name, hostname, workdir);
+    /* Cuando se pone un color, lo que se sigue imprimiendo, se imprime con ese color,
+       por ende, para usar el color por defecto hay que usar ANSI_NOCOLOR */
     fflush(stdout);
 }
