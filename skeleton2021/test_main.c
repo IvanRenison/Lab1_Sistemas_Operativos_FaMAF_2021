c3c996172500c94d753df554e175b2f28aea3962
Iván <ivan.renison@mi.unc.edu.ar>
2021-09-07 18:42:09 -0300

    Comentado main de test_main

--- skeleton2021/test_main.c
+++ skeleton2021/test_main.c
@@ -14,63 +14,10 @@
 #include "prompt.h"
 
 
-static void test_prompt(void){
-    printf("test_prompt...\n");
-    show_prompt();
-}
 
-static void test_pipelines(void){
-    printf("test_pipelines..\n");
-    // scommand comando1 = scommand_new();
-    // scommand_push_back(comando1, strdup("pi"));
-    // scommand_push_back(comando1, strdup("1000000000"));
 
-    // // scommand comando1 = scommand_new();
-
-    // // scommand comando2 = scommand_new();
-    // // scommand_push_back(comando2, strdup("ls"));
-
-    // // scommand comando3 = scommand_new();
-    // // scommand_push_back(comando3, strdup("wc"));
-    // // scommand_push_back(comando3, strdup("-l"));
-
-    // pipeline p = pipeline_new();
-    // pipeline_set_wait(p, true);
-    // pipeline_push_back(p, comando1);
-    // pipeline_push_back(p, comando2);
-    // pipeline_push_back(p, comando3);
-    // // pipeline p = pipeline_new();
-    // // pipeline_push_back(p, comando1);
-    // // pipeline_push_back(p, comando2);
-    // // pipeline_push_back(p, comando3);
-
-    // // execute_multiple_pipelines(p);
-
-    // // p = pipeline_destroy(p);
-
-    // char** argv = calloc(3, sizeof(char));
-    // argv[0] = strdup("ctweqewtqd");
-    // argv[1] = strdup("dafsf");
-    // argv[2] = NULL;
-    // execvp(argv[0], argv);
-
-    // pipeline pipe = pipeline_new();
-    // scommand comando1 = scommand_new();
-    // scommand comando2 = scommand_new();
-    // scommand comando3 = scommand_new();
-    // scommand_push_back(comando1, strdup("ls"));
-    // scommand_push_back(comando2, strdup("grep"));
-    // scommand_push_back(comando2, strdup("c"));
-    // scommand_push_back(comando3, strdup("sort"));
-    // pipeline_push_back(pipe, comando1);
-    // pipeline_push_back(pipe, comando2);
-    // pipeline_push_back(pipe, comando3);
-    // multiple_pipelines(pipe);
-    // pipeline_destroy(pipe);
-}
-
-int main(void){
+/* int main(void){
     test_prompt();
     test_pipelines();
     return (EXIT_SUCCESS);
-}
+} */
