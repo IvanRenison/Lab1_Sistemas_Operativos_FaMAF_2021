234181c1710488c303915b0d499c039e1b324fab
Iván <ivan.renison@mi.unc.edu.ar>
2021-09-07 18:41:31 -0300

    Emprolijamiento mybash.c

--- skeleton2021/mybash.c
+++ skeleton2021/mybash.c
@@ -9,32 +9,30 @@
 #include "parser.h"
 #include "prompt.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[]) {
 
     // Inicializo exit_from_mybash para que no salga
     exit_from_mybash = false;
 
-    Parser parser;
-    pipeline pipe;
+    Parser parser = parser_new(stdin);
 
-    parser = parser_new(stdin);
     while (!exit_from_mybash) {
         // exit_from_mybash es una variable global declarada en builtin.h
         show_prompt();
-        pipe = parse_pipeline(parser);
+        pipeline apipe = parse_pipeline(parser);
 
         /* Si se llegó a un final de archivo siginifca que hay que salir después
            de ejecutar el comando */
         exit_from_mybash = parser_at_eof(parser);
 
-        if (pipe != NULL) {
-            execute_pipeline(pipe);
-            pipeline_destroy(pipe);
-        } else if (!exit_from_mybash && pipe != NULL) {
-            printf("Comando no valido\n");
+        if (apipe != NULL) {
+            execute_pipeline(apipe);
+            apipe = pipeline_destroy(apipe);
         }
     }
-    parser_destroy(parser);
-    parser = NULL;
+
+    // Antes de salir se imprime un salto de linea
+    printf("\n");
+    parser = parser_destroy(parser);
     return 0;
-} 
+}
